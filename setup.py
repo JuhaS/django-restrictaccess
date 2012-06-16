bb233218c4206f6b13d9bcc48aad50d341dde6b6
Juha <suomalainen.juha@gmail.com>
2012-06-16 20:58:19 +0300

    Try fixes

--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@
 from distutils.core import setup
 
 setup(
-    name='django-restrictaccess',
+    name='djrestrictaccess',
     version='0.0.3',
     author=u'Juha Suomalainen',
     author_email='',
-    packages=['djrestrictaccess'],
+    packages=find_packages(),
     package_data={'djrestrictaccess': ['templates/*.html']},
     url='https://github.com/JuhaS/django-restrictaccess',
     license='BSD licence, see LICENCE.txt',
     description='Django library that allows to restrict access (user needs a key) to any django site in a plug-n-play fashion.',
     long_description=open('README.md').read(),
     zip_safe=False,
+)
+
+from setuptools import setup, find_packages
+setup(
+    name = "djrestrictaccess",
+    version = "0.0.4",
+    packages = 
 )
