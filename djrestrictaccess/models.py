4a2087322a0a2b19535b0e14b1ff9d833e54de00
Juha <suomalainen.juha@gmail.com>
2012-06-16 21:18:55 +0300

    Bug fix on field size

--- djrestrictaccess/models.py
+++ djrestrictaccess/models.py
@@ -10,5 +10,5 @@ class WhitelistedSession(models.Model):
 
 
 class AccessKey(models.Model):
-    key = models.CharField(max_length=10)
+    key = models.CharField(max_length=20)
     accessesLeft = models.IntegerField(default=1)
