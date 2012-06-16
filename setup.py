546a827e12488cda7343b4dec7bc4d2702139fba
JuhaS <suomalainen.juha@gmail.com>
2012-06-16 19:43:19 +0300

    Update master

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-restrictaccess',
-    version='0.0.1',
+    version='0.0.2',
     author=u'Juha Suomalainen',
     author_email='',
     packages=['djrestrictaccess'],
